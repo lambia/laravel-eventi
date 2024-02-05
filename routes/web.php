4ddca897cb439ee6480b81ddaa5bb61214a35a94
Luca Lambiase <luca.lambia@gmail.com>
2024-02-05 12:29:10 +0100

    feat(scaffold): create admin namespace for dashboard

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth'])
+    ->prefix('admin') //definisce il prefisso "admin/" per le rotte di questo gruppo
+    ->name('admin.') //definisce il pattern con cui generare i nomi delle rotte cioè "admin.qualcosa"
+    ->group(function () {
 
-require __DIR__.'/auth.php';
+        //Siamo nel gruppo quindi:
+        // - il percorso "/" diventa "admin/"
+        // - il nome della rotta ->name("dashboard") diventa ->name("admin.dashboard")
+        Route::get('/', function () {
+            return view("admin.dashboard");
+        })->name('dashboard');
+    });
+
+require __DIR__ . '/auth.php';
