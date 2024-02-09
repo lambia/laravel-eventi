c7f3262c891a3d74ac8e2660d33423920039e17b
Luca Lambiase <luca.lambia@gmail.com>
2024-02-09 16:39:15 +0100

    feat(api): simplify index

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -11,7 +11,7 @@ class EventController extends Controller
     //
     public function index()
     {
-        $results = Event::with("user")->get();
+        $results = Event::all();
         $data = [
             "success" => true,
             "payload" => $results
